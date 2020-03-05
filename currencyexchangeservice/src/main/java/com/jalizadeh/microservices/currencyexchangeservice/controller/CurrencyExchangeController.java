4429f7c6b76964df359faefa06f222a2609ccfac
j-alizadeh <jalizade3d@yahoo.com>
2020-03-05 18:14:35 +0100

    Setting up Dynamic Port in the the Response

--- currencyexchangeservice/src/main/java/com/jalizadeh/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currencyexchangeservice/src/main/java/com/jalizadeh/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 
 import javax.websocket.server.PathParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +16,15 @@ import com.jalizadeh.microservices.currencyexchangeservice.model.ExchangeValue;
 @RestController
 public class CurrencyExchangeController {
 	
+	@Autowired
+	private Environment environment;
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(1.12));
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, 
+			@PathVariable String to) {
+		ExchangeValue ev = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(1.12));
+		ev.setPort(Integer.parseInt(environment.getProperty("local.server.port")));		
+		return ev;
 	}
 
 }
