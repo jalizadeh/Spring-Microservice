286f7fae5860cfd56497412b5eb1796e26d23fe5
j-alizadeh <jalizade3d@yahoo.com>
2020-03-05 12:49:49 +0100

    App Configuration

--- src/main/java/com/jalizadeh/microservices/limits_service/controller/LimitsConfigurationController.java
+++ src/main/java/com/jalizadeh/microservices/limits_service/controller/LimitsConfigurationController.java
@@ -1,16 +1,21 @@
 package com.jalizadeh.microservices.limits_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jalizadeh.microservices.limits_service.bean.LimitConfiguration;
+import com.jalizadeh.microservices.limits_service.configuration.Configuration;
 
 @RestController
 public class LimitsConfigurationController {
 	
+	@Autowired
+	private Configuration config;
+	
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfiguration() {
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(config.getMaximum(), config.getMinimum());
 	}
 
 }
